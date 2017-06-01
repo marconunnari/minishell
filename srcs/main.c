e496a4a19474593854e4b7694391bec75e9d3b22
Marco Nunnari <marnunna@gmail.com>
2017-06-02 00:53:36 +0200

    ok

--- srcs/main.c
+++ srcs/main.c
@@ -1,27 +1,31 @@
 #include "minishell.h"
 
+void	exec_cmd(char *cmd)
+{
+	pid_t	child_pid;
+	int	stat_loc;
+
+	if ((child_pid = fork()) < 0)
+		ft_error("fork error", 1);
+	else if (child_pid == 0)
+	{
+		ft_printfnl("exec %s", cmd);
+		exit(1);
+	}
+	else
+	{
+		//todo: background
+		waitpid(child_pid, &stat_loc, WUNTRACED);
+		ft_printf("$> ");
+	}
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	*cmd;
-	pid_t	child_pid;
-	int	stat_loc;
 
 	(void)argc;(void)argv;(void)env;
 	ft_printf("$> ");
 	while (get_next_line(0, &cmd) > 0)
-	{
-		child_pid = fork();
-		if (child_pid == 0)
-		{
-			ft_printfnl("%s executed", cmd);
-			break;
-		}
-		else if (child_pid > 0)
-		{
-			waitpid(child_pid, &stat_loc, WUNTRACED);
-			ft_printf("$> ");
-		}
-		else
-			ft_error("fork error", 1);
-	}
+		exec_cmd(cmd);
 }

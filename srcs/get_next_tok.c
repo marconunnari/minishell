4ba6eb62934f034f09c0f4923bef0b903bd1d9b6
Marco Nunnari <marnunna@gmail.com>
2017-06-26 15:31:56 +0200

    refactoring get_next_tok

--- srcs/get_next_tok.c
+++ srcs/get_next_tok.c
@@ -12,10 +12,11 @@
 
 #include "minishell.h"
 
-static char	is_quote(char c)
+/*
+static int	is_quote(char c)
 {
 	if (c == '\"' || c == '\'')
-		return (c);
+		return (1);
 	return (0);
 }
 
@@ -24,68 +25,26 @@ static int	missing(char c)
 	ft_printfnl("Unmatched %c", c);
 	return (-1);
 }
+*/
 
-static void	get_quoted(char *str, char quot, char **arg, int *i, char **env)
+static int	process_str(char **str, char **tok, char **env)
 {
-	char			*tmp;
-	char			*var;
-	char			*newarg;
+	char	*ptr;
 
-	tmp = ft_strsub(str, 0, ft_strchr(str, quot) - str);
-	if (quot == '\"' && tmp[0] == '$' && tmp[1])
-	{
-		var = get_envar(&tmp[1], env);
-		newarg = ft_strnew(ft_strlen(*arg) + ft_strlen(var) + ft_strlen(str) + 1);
-		ft_strcpy(newarg, *arg);
-		ft_strcpy(&newarg[ft_strlen(*arg)], var);
-		free(*arg);
-		*arg = newarg;
-		*i += ft_strlen(var);
-	}
-	else
-	{
-		ft_strcpy(&(arg[0][*i]), tmp);
-		*i += ft_strlen(tmp);
-	}
-	free(tmp);
-}
-
-static int	process_str(char **str, char **arg, char **env)
-{
-	char			quot;
-	int				i;
-
-	i = 0;
-	while (**str)
-	{
-		if ((quot = is_quote(**str)))
-		{
-			*str += 1;
-			if (!ft_strchr(*str, quot))
-				return (missing(quot));
-			else
-				get_quoted(*str, quot, arg, &i, env);
-			*str = ft_strchr(*str, quot) + 1;
-			if (**str == ' ')
-				return (1);
-			else
-				continue;
-		}
-		if (**str == ' ')
-			return (1);
-		arg[0][i++] = **str;
-		*str += 1;
-	}
+	(void)env;
+	if ((ptr = ft_strchr(*str, ' ')))
+		*ptr = '\0';
+	*tok = (*str);
+	*str += ft_strlen(*tok);
 	return (1);
 }
 
-int		get_next_tok(char **str, char **arg, char **env)
+int		get_next_tok(char **str, char **tok, char **env)
 {
-	(void)env;
 	while (**str && **str == ' ')
 		*str += 1;
 	if (**str == '\0')
 		return (0);
-	*arg = ft_strnew((ft_strlen(*str) + 1));
-	return (process_str(str, arg, env));
+	*tok = ft_strnew(0);
+	return (process_str(str, tok, env));
 }

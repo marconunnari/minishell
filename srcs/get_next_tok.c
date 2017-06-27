7542be1b0f0b538d0a847c261b776c0500fde35c
Marco Nunnari <marnunna@gmail.com>
2017-06-27 20:38:13 +0200

    ok

--- srcs/get_next_tok.c
+++ srcs/get_next_tok.c
@@ -28,36 +28,37 @@ static int	missing(char c)
 	return (-1);
 }
 
-static int	totok(char **tok, char *piece)
+static void	totok(char **tok, char *piece)
 {
 	char	*tmp;
 
-	if (!piece)
-		return (0);
 	tmp = *tok;
 	*tok = ft_strjoin(tmp, piece);
 	free(tmp);
-	return (1);
 }
 
-static char	*take_string(char *ptr1, char *ptr2, char **env, char **str)
+static int	take_string(char *ptr1, char *ptr2, char **env, char **tok)
 {
 	char		*envar;
+	char		*ptr3;
 
 	if (ptr2)
 		*ptr2 = '\0';
 	else
 		ptr2 = ptr1 + ft_strlen(ptr1) - 1;
-	if (*ptr1 == '$' && env)
+	if (env && (ptr3 = ft_strchr(ptr1, '$')))
 	{
-		envar = get_envar(ptr1 + 1, env);
+		if (ptr3 > ptr1)
+			take_string(ptr1, ptr3, env, tok);
+		envar = get_envar(ptr3 + 1, env);
 		if (!envar)
-			ft_printfnl("%s: Undefined variable.", ptr1 + 1);
+			ft_printfnl("%s: Undefined variable.", ptr3 + 1);
 		ptr1 = envar;
 	}
-	if (str)
-		*str = ptr2 + 1;
-	return (ptr1);
+	if (!ptr1)
+		return (0);
+	totok(tok, ptr1);
+	return (1);
 }
 
 static int	process_str(char **str, char **tok, char **env)
@@ -74,19 +75,21 @@ static int	process_str(char **str, char **tok, char **env)
 			break ;
 		quot = *ptr1;
 		if (ptr1 > *str)
-			if (!totok(tok, take_string(*str, ptr1, env, str)))
+		{
+			if (!take_string(*str, ptr1, env, tok))
 				return (-1);
+			*str = ptr1 + 1;
+		}
 		ptr2 = ft_strchr(ptr1 + 1, quot);
 		if (!ptr2 || (*ptr2 != quot))
 			return (missing(quot));
-		if (*ptr1 == '\'')
-			totok(tok, take_string(ptr1 + 1, ptr2, NULL, str));
-		else
-			if (!totok(tok, take_string(ptr1 + 1, ptr2, env, str)))
-				return (-1);
+		if (!take_string(ptr1 + 1, ptr2, *ptr1 == '\'' ? NULL : env, tok))
+			return (-1);
+		*str = ptr2 + 1;
 	}
-	if (!totok(tok, take_string(*str, ptr2, env, str)))
+	if (!take_string(*str, ptr2, env, tok))
 		return (-1);
+	*str = ptr2 + 1;
 	return (1);
 }
 
